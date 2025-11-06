1582191a5ae0827f25051bd1864e64e3567f5a62
manojbelbase <manojbelbase56@gmail.com>
2025-11-06 18:45:41 +0545

    fix(date): correct bs to ad conversion edge case
    - fix the year conversion issue in bsToAd function
    - address the bug where the year was incorrectly calculated for certain BS dates

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   const ad = adToBs('2025-10-17');
   console.log('Ad to Bs conversion:', ad);
 
-  const bs = bsToAd('2095-12-30');
+  const bs = bsToAd('2099-12-30');
   console.log('Bs to Ad conversion:', bs.formatted);
   return (
     <div>
